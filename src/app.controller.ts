925b721458f4fd90419caca859f7fd369496b972
Edward <edwardduong71311@gmail.com>
2024-09-10 22:25:29 -0500

    feat(ERP-17): add helmet & rate limiter and stuff

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 
 @Controller()
+@UseGuards(ThrottlerGuard)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Throttle({ default: { limit: 10, ttl: 6000 } })
   @Get()
   getHello(): string {
     return this.appService.getHello();
